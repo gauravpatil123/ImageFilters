d270fa72aca3c92843532cb906132e9a14b2e19c
gauravpatil123 <gaurav.patil123@gmail.com>
2024-09-11 19:34:56 +0530

    initialised command choice function

--- flist.py
+++ flist.py
@@ -6,6 +6,15 @@ FEATURE_LIST = ["grayScale", "Purple", "Saffron", "Color",
                 "Side_Mirror", "Top_Mirror", "Contrast", "Indian_Flag", 
                 "RandomPix", "RandomPix2", "SwitchPix", "SwitchRandPix"]
 
+def command_choice(flist):
+    print("Do you want conversion commands? (Y/N)")
+    ans = str(input())
+    if ans == "Y":
+        counter = 1
+        for filter in flist:
+            print(counter+")"+filter)
+    elif ans == "N":
+        print("END")
 
 def main(flist=FEATURE_LIST):
     print("Filters for Image conversion:")
