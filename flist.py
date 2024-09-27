bf5ba02a53851d1ef2650d168db8d40ded2177b2
gauravpatil123 <gaurav.patil123@gmail.com>
2024-09-27 17:15:31 +0530

    cleaned choice command fxn

--- flist.py
+++ flist.py
@@ -17,34 +17,29 @@ def create_fdict(flist=FEATURE_LIST):
     for idx in range(len(flist)):
         id = idx + 1
         FEATURE_DICT[id] = flist[idx]
-        #print(str(id)+" "+flist[idx]) #DONOT UNCOMMENT THIS IS ONLY FOR TESTING
 
 def command_choice(flist:list):
     global FEATURE_DICT
-    print("Do you want conversion commands? (Y/N)")
-    inp = str(input())
-    if inp == "Y":
-        counter = 1
-        for filter in flist:
-            print(str(counter)+") java ImageStyle "+filter)
-            counter += 1
-        choice = input("Input filter choice number for image conversion.\n")
-        slected_filter = FEATURE_DICT[int(choice)]
-        conv_command = "java ImageStyle "+ slected_filter
-        print("Chosen filter is "+slected_filter+"\nDo you want to proceed? (Y/N)")
-        proceed = str(input())
-        if proceed == "Y":
-            os.system(conv_command)
-        elif proceed == "N":
-            print("END")
-    elif inp == "N":
+    counter = 1
+    for filter in flist:
+        # print(str(counter)+") java ImageStyle "+filter)
+        print(str(counter)+") "+filter)
+        counter += 1
+    choice = input("Input filter choice number for image conversion.\n")
+    slected_filter = FEATURE_DICT[int(choice)]
+    conv_command = "java ImageStyle "+ slected_filter
+    print("Chosen filter is "+slected_filter+"\nDo you want to proceed? (Y/N)")
+    proceed = str(input())
+    if proceed == "Y":
+        os.system(conv_command)
+    elif proceed == "N":
         print("END")
 
 def main(flist=FEATURE_LIST):
     create_fdict()
     print("Filters for Image conversion:")
-    for filter in flist:
-        print(filter)
+    # for filter in flist:
+    #     print(filter)
     command_choice(flist)
 
 
