99ef82fcdddb5ee725f2c9218190f0a5544db718
gauravpatil123 <gaurav.patil123@gmail.com>
2024-10-10 16:28:48 +0530

    initialised constructor in hideimage

--- HideImage.java
+++ HideImage.java
@@ -11,7 +11,19 @@ public class HideImage {
     private int Fheight;
     private int Bwidth;
     private int Bheight;
-    
+
+    public HideImage(ImageResource FImage, ImageResource BImage) {
+
+        this.frontImage = FImage;
+        this.backImage = BImage;
+        this.Fwidth = FImage.getWidth();
+        this.Fheight = FImage.getHeight();
+        this.Bwidth = BImage.getWidth();
+        this.Bheight = BImage.getHeight();
+        this.HiddenImage = new ImageResource(Fwidth, Fheight);
+
+    }
+
     public static void main(String[] args) {
 
     }
