36834882c6cf0757952df80f4d4c7b2cc7b8afbc
gauravpatil123 <gaurav.patil123@gmail.com>
2024-10-10 16:27:33 +0530

    initialised class variables for hideimage

--- HideImage.java
+++ HideImage.java
@@ -4,8 +4,16 @@ import java.io.*;
 
 public class HideImage {
     
+    private ImageResource frontImage;
+    private ImageResource backImage;
+    private ImageResource HiddenImage;
+    private int Fwidth;
+    private int Fheight;
+    private int Bwidth;
+    private int Bheight;
+    
     public static void main(String[] args) {
-        
+
     }
 
 }
