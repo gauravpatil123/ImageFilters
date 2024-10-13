9d3a932eed47a820e7951855838c8ea8fd39edda
gauravpatil123 <gaurav.patil123@gmail.com>
2024-10-13 16:22:00 +0530

    added todo to hidecolor

--- HideImage.java
+++ HideImage.java
@@ -49,6 +49,7 @@ public class HideImage {
     }
 
     private int hideColor (int FrontColor, int BackColor) {
+        //TODO: Add documentation
 
         int newColor = (int) ((Math.floor(FrontColor/16)*16) + Math.floor(BackColor/16));
         return newColor;
