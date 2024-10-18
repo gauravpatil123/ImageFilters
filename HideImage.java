bed018519276782bfa5cfd1aaab451f2c9db2ad8
gauravpatil123 <gaurav.patil123@gmail.com>
2024-10-18 19:51:29 +0530

    initialised unhide color image

--- HideImage.java
+++ HideImage.java
@@ -74,6 +74,10 @@ public class HideImage {
     //NOTE: write binary math for decryption
     //TODO: use args in main fxn to determine encryption or decryption
 
+    private int unhideColor (int PixColor) {
+        
+    }
+
     public static void main(String[] args) {
 
         String arg = args[0];
