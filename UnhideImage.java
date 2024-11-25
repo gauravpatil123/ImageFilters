aa157799170929a20e4d84796099303b44bfff65
gauravpatil123 <gaurav.patil123@gmail.com>
2024-11-25 20:55:09 +0530

    added doc stricg in hideimage

--- UnhideImage.java
+++ UnhideImage.java
@@ -3,7 +3,7 @@
  *  -> Unhide class is work in progress + not tested
  *  -> Hide class is work in progress + not tested
  *  -> Mention incomplete classes in README
- *  -> Mention project status as halted
+ *  -> Mention project status as halted and futire directions
 */
 
 import edu.duke.*;
