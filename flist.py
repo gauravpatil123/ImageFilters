a3ea182b4cb180ad1f2b4896fa91039e6887eb31
gauravpatil123 <gaurav.patil123@gmail.com>
2024-12-09 18:18:17 +0530

    cleaned command choice code

--- flist.py
+++ flist.py
@@ -46,12 +46,6 @@ def command_choice(flist:list) -> None:
     display_filters(counter, flist)
     choice = authenticate_choice(FEATURE_DICT)
     execute_choice(choice)
-    # proceed = choice[0]
-    # if proceed == "Y":
-    #     conv_command = choice[1]
-    #     os.system(conv_command)
-    # elif proceed == "N":
-    #     print("END")
     #TODO: add one more else condition to hande other inputs
 
 def main(flist:list=FEATURE_LIST) -> None:
